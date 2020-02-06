b27504565e4a34953c62095b89c5f499bc27ccb5
g4rcez <allan.f.garcez@gmail.com>
2020-02-06 00:57:29 -0300

    enum for error messages

---
+++ packages/common/src/data/index.ts
@@ -0,0 +1,7 @@
+export enum MessageMap {
+  newChatExistError = "chat/chat-already-exist",
+  userAlreadyInChat = "chat/user-already-in-chat",
+  userAlreadyRegister = "user/user-already-register",
+  registerEmptyEmailValidation = "user/register-empty-email",
+  registerInvalidEmailValidation = "user/register-invalid-email"
+}
