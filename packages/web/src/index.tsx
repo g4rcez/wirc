aabed73923b4016b237b325e90e78beeb34f2c75
g4rcez <allan.f.garcez@gmail.com>
2020-02-09 18:42:45 -0300

    react entrypoint

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import socket from "socket.io-client";
+import * as serviceWorker from "./serviceWorker";
+import "./styles/index.css";
+import App from "./wirc";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const io = socket("http://localhost:3000", {
+  randomizationFactor: 5,
+  rememberUpgrade: true
+});
+
+const Wirc = () => {
+  return <App />;
+};
+
+ReactDOM.render(<Wirc />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
