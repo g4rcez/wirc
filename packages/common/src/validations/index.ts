f1f2c446348c9325f491d93876ccac890bf98a5d
g4rcez <allan.f.garcez@gmail.com>
2020-02-09 18:33:30 -0300

    user validations

--- packages/common/src/validations/index.ts
+++ packages/common/src/validations/index.ts
@@ -1,13 +1,40 @@
+type ObjectValidate = { valid: boolean; messages: string[] };
+
 export type Validations<T> = {
-  [key in keyof T]: (value: T[key], object: T) => boolean;
+  [key in keyof T]: (value: T[key], object: T) => ObjectValidate;
 };
 
-export const validator = <T>(data: T, validations: Validations<T>) => {
-  return Object.entries(
-    validations
-  ).every(([key, validation]: [string, unknown]) =>
-    (validation as Function)(data[key], data)
+type Validate<T> = {
+  valid: boolean;
+  validations: {
+    [key in keyof Partial<T>]: ObjectValidate & { key: keyof T };
+  }[];
+};
+
+export type ValidationsType<T> = {
+  [key in keyof Partial<T>]: ObjectValidate & {
+    key: keyof T;
+  };
+};
+
+export const validator = <T>(
+  data: T,
+  validations: Validations<T>
+): Validate<T> => {
+  const map = Object.entries(validations).map(
+    ([key, validation]: [string, unknown]) => {
+      const validateData = (validation as Function)(data[key], data);
+      return { ...validateData, key };
+    }
   );
+  const valid = map.every(x => x.valid);
+  const onlyWrongValidations = map.reduce((acc, el) => {
+    if (el.valid) {
+      return acc;
+    }
+    return [...acc, el];
+  }, []);
+  return { valid, validations: onlyWrongValidations };
 };
 
 const nicknameRegex = /^[a-z][a-zA-Z_0-9-]{2,31}$/;
