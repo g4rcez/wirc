fb6dfcf1f4a2fbfce8dbb0cdec7717d3e05e06d8
g4rcez <allan.f.garcez@gmail.com>
2020-02-09 18:20:58 -0300

    add /search route

--- packages/api/src/controller/user.controller.ts
+++ packages/api/src/controller/user.controller.ts
@@ -1,5 +1,23 @@
 import { Router } from "express";
+import { User } from "@wirc/common";
+import userService from "../services/user.service";
 
-const userRegisterRouter = Router();
+const userController = Router();
 
-export default userRegisterRouter;
+userController.post("/register", (req, res) => {
+  const register = userService.register(req.body);
+  if (register instanceof User) {
+    return res.status(201).send(register);
+  }
+  if (register === false) {
+    return res.status(500).send();
+  }
+  return res.status(406).send(register);
+});
+
+userController.get("/search", (req, res) => {
+  const { email } = req.query;
+  res.send({ exist: userService.getUserByEmail(email) !== undefined });
+});
+
+export default userController;
