20c6de5734598e5b4f6136be06e76e58374a2303
g4rcez <allan.f.garcez@gmail.com>
2020-02-09 18:33:50 -0300

    add exports

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,13 @@
 export * from "./data";
 export * from "./validations";
+export * from "./domain/chat/chat";
+export * from "./domain/chat/new-chat-exist-error";
+export * from "./domain/user/user";
+export * from "./domain/user/user-already-in-chat";
+export * from "./domain/user/user-already-register";
+
+export const uuid = (): string =>
+  //@ts-ignore
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (a: any) =>
+    (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+  );
