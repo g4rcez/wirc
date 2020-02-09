1a92143664de8f9ffa53edc0deca1312af960200
g4rcez <allan.f.garcez@gmail.com>
2020-02-09 18:22:39 -0300

    move to @common

--- packages/api/src/domain/chat/new-chat-exist-error.ts
+++
@@ -1,8 +0,0 @@
-import { MessageMap } from "@wirc/common";
-
-export class NewChatExistError extends Error {
-  constructor() {
-    super(MessageMap.newChatExistError);
-    this.name = "NewChatExistError";
-  }
-}
