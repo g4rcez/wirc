157c8935ecf31ed23f21d112b8b730639ccf37f1
g4rcez <allan.f.garcez@gmail.com>
2020-02-09 18:25:08 -0300

    user services - register and search

--- packages/api/src/services/user.service.ts
+++ packages/api/src/services/user.service.ts
@@ -1,17 +1,31 @@
+import {
+  User,
+  UserValidatorSchema,
+  ValidationsType,
+  validator
+} from "@wirc/common";
 import { RegisterData, userRepository } from "../repository/user.repository";
-import { UserValidatorSchema, User } from "../domain/user/user";
-import { validator } from "@wirc/common";
 
-const register = (registerUser: RegisterData): User | false => {
+const register = (
+  registerUser: RegisterData
+): User | false | ValidationsType<User>[] => {
   const isValidUser = validator(registerUser, UserValidatorSchema);
-  if (isValidUser) {
-    if (userRepository.existUserByEmail(registerUser.email)) {
-      return userRepository.register(registerUser);
+  if (isValidUser.valid) {
+    if (!userRepository.existUserByEmail(registerUser.email)) {
+      try {
+        return userRepository.register(registerUser);
+      } catch (error) {
+        console.log("ERROR", error);
+        return false;
+      }
     }
   }
-  return false;
+  return isValidUser.validations;
 };
 
+const getUserByEmail = (email: string) => userRepository.findUserByEmail(email);
+
 export default {
-  register
+  register,
+  getUserByEmail
 };
